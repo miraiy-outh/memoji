5d061f09a7bf4074940d9d789f7a7e32c3152b03
Roman Surinov <romasurinov13@mail.ru>
2023-08-16 00:15:44 +0300

    добавил сборщик vite, написал скрипт для запуска

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+    },
+    build: {
+        outDir: 'dist'
+    },
+    watch: {
+        include: 'src/**/*'
+    }
+}
